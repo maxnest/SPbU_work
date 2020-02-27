9d02dfeaad00f0e27ad085306f2a3f610e533b48
Maksim Nesterenko <maxnest@ro.ru>
2020-02-27 21:32:43 +0300

    Add files via upload

--- Psilostomatidae_sets_of_all_active_genes.py
+++ Psilostomatidae_sets_of_all_active_genes.py
@@ -7,23 +7,24 @@ except ImportError:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--table', type=argparse.FileType('r'), required=True)
+parser.add_argument('--threshold', type=int, required=True, help="The minimal expression level")
 parser.add_argument('--out', type=str, required=True)
 args = parser.parse_args()
 
 
-def table_parsing(table, rediae, cercariae, marita):
+def table_parsing(table, rediae, cercariae, marita, threshold):
     header = table.readline()
     for line in table:
         description = line.strip().split("\t")
         contig_ID, red_tpm, cer_tpm, mar_tpm = description[0], float(description[1]), float(description[2]), \
                                                float(description[3])
-        if red_tpm >= 1:
+        if red_tpm > threshold:
             rediae.append(contig_ID)
 
-        if cer_tpm >= 1:
+        if cer_tpm > threshold:
             cercariae.append(contig_ID)
 
-        if mar_tpm >= 1:
+        if mar_tpm > threshold:
             marita.append(contig_ID)
 
 
@@ -35,7 +36,7 @@ def output_writing(out, list, tag):
 
 if __name__ == "__main__":
     rediae, cercariae, marita = [], [], []
-    table_parsing(args.table, rediae, cercariae, marita)
+    table_parsing(args.table, rediae, cercariae, marita, args.threshold)
     output_writing(args.out, rediae, "rediae")
     output_writing(args.out, cercariae, "cercariae")
     output_writing(args.out, marita, "marita")
