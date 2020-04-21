a178ab7ad8a4d4ee16cae6efe347ffbfed98df04
Maksim Nesterenko <maxnest@ro.ru>
2020-04-21 23:37:19 +0300

    Add files via upload

---
+++ Psilostomatidae_KEGG_summary_EC.py
@@ -0,0 +1,83 @@
+try:
+    import argparse
+except ImportError:
+    print("Please check if module 'argparse' is installed")
+    quit()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--summary_table', type=argparse.FileType('r'), required=True,
+                    help="Summary table with results of analyses: good.*_ref.genes_level.summary_table.tsv")
+parser.add_argument('--kegg_description', type=argparse.FileType('r'), required=True,
+                    help="Table with description of KEGG pathways: Super-pathway(Category), Code, Description")
+parser.add_argument('--output', type=str, required=True)
+args = parser.parse_args()
+
+
+def kegg_parsing(kegg_description, kegg_dict):
+    header = kegg_description.readline()
+    for line in kegg_description:
+        description = line.strip().split("\t")
+        category, code, pathway_description = description[0], description[1], description[2]
+        if category not in kegg_dict.keys():
+            kegg_dict[category] = {}
+
+        kegg_dict[category][code] = {"Description": pathway_description,
+                                     "Redia": [], "Cercaria": [], "Marita": [], "Total_count": []}
+
+
+def summary_table_parsing(summary_table, kegg_dict):
+    header = summary_table.readline()
+    for line in summary_table:
+        description = line.strip().split("\t")
+        gene, ec_number,  kegg_pathways, redia_exp, cercaria_exp, marita_exp = description[0], description[13],  \
+            description[15], float(description[26]), float(description[27]), float(description[28])
+
+        for kegg_pathway in kegg_pathways.split(","):
+            if kegg_pathway.startswith("ko"):
+                for category, codes in kegg_dict.items():
+                    if kegg_pathway.split("ko")[1] in codes:
+                        if ec_number != "-":
+                            codes[kegg_pathway.split("ko")[1]]["Total_count"].append(ec_number)
+
+                            if redia_exp >= 1:
+                                codes[kegg_pathway.split("ko")[1]]["Redia"].append(ec_number)
+
+                            if cercaria_exp >= 1:
+                                codes[kegg_pathway.split("ko")[1]]["Cercaria"].append(ec_number)
+
+                            if marita_exp >= 1:
+                                codes[kegg_pathway.split("ko")[1]]["Marita"].append(ec_number)
+
+
+def output_writing(output, kegg_dict):
+    with open("{output}.all_pathways_with_5_or_more_enzymes.tsv".format(output=output), 'a') as all_active_pathways:
+        all_active_pathways.write("Category\tPathway\tRedia:EnzymesCount\tCercaria:EnzymesCount\tMarita:EnzymesCount\t"
+                                  "Total_EnzymesCount\tRedia:Percent\tCercaria:Percent\tMarita:Percent\n")
+        for category, codes in kegg_dict.items():
+            for code, values in codes.items():
+                if len(values["Total_count"]) >= 5:
+                    all_active_pathways.write("{category}\t{pathway}\t{redia_count}\t{cercaria_count}\t{marita_count}\t"
+                                              "{total}\t{redia_percent}\t{cercaria_percent}\t{marita_percent}\n".format(
+                                                category=category,
+                                                pathway="{code}|{description}".format(code=code,
+                                                                                      description=values["Description"]),
+                                                redia_count=len(set(values["Redia"])),
+                                                cercaria_count=len(set(values["Cercaria"])),
+                                                marita_count=len(set(values["Marita"])),
+                                                total=len(set(values["Total_count"])),
+                                                redia_percent=
+                                                round((len(set(values["Redia"]))/len(set(values["Total_count"]))) * 100,
+                                                      2),
+                                                cercaria_percent=
+                                                round((len(set(values["Cercaria"]))/len(set(values["Total_count"]))) * 100,
+                                                      2),
+                                                marita_percent=
+                                                round((len(set(values["Marita"]))/len(set(values["Total_count"]))) * 100,
+                                                      2)))
+
+
+if __name__ == "__main__":
+    kegg_dict = {}
+    kegg_parsing(args.kegg_description, kegg_dict)
+    summary_table_parsing(args.summary_table, kegg_dict)
+    output_writing(args.output, kegg_dict)
