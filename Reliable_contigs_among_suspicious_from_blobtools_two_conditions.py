641e4a5e8c14467b060ca817a8253f6468388617
Maksim Nesterenko <maxnest@ro.ru>
2020-05-08 21:37:48 +0300

    Add files via upload

--- Reliable_contigs_among_suspicious_from_blobtools_two_conditions.py
+++ Reliable_contigs_among_suspicious_from_blobtools_two_conditions.py
@@ -20,34 +20,42 @@ args = parser.parse_args()
 def blast_tab_parsing(blast_tab, blast_dict):
     for line in blast_tab:
         description = line.strip().split("\t")
-        qseqid, sseqid, pident, length = description[0], description[1], float(description[2]), float(description[4])
-        blast_dict[qseqid] = {"sseqid": sseqid, "pident": pident, "length": length}
+        qseqid, sseqid, pident, length = description[0], description[1], float(description[2]), float(description[3])
+        if qseqid not in blast_dict:
+            blast_dict[qseqid] = []
+
+        blast_dict[qseqid].append({"sseqid": sseqid, "pident": pident, "length": length})
 
 
 def contigs_selection(contigs_dict, ref_dict, uniprot_dict, reliable):
     for contig in contigs_dict.keys():
         if contig in ref_dict.keys() and contig in uniprot_dict.keys():
-            if ref_dict[contig]["pident"] > uniprot_dict[contig]["pident"] or ref_dict[contig]["length"] > uniprot_dict[contig]["length"]:
+            ref_pident = [el["pident"] for el in ref_dict[contig]]
+            uniprot_pident = [el["pident"] for el in uniprot_dict[contig]]
+            ref_length = [el["length"] for el in ref_dict[contig]]
+            uniprot_length = [el["length"] for el in uniprot_dict[contig]]
+            if max(ref_pident) > max(uniprot_pident) or max(ref_length) > max(uniprot_length):
                 reliable.append(contig)
     print("### Number of reliable contigs: {len} ###".format(len=len(set(reliable))))
 
 
-def output_writing(output, reliable, contigs_dict, ref_dict, uniprot_dict):
+def output_writing(output, reliable, ref_dict, uniprot_dict):
     with open("{output}.IDs_of_all_reliable_contigs.txt".format(output=output), 'a') as reliable_txt:
         for contig in set(reliable):
             reliable_txt.write("{contig}\n".format(contig=contig))
 
-    with open("{output}.all_reliable_contigs.fasta".format(output=output), 'a') as reliable_fasta:
-        for contig in set(reliable):
-            reliable_fasta.write(">{id}\n{seq}\n".format(id=contig, seq=contigs_dict[contig]))
-
     with open("{output}.all_reliable_contigs_alignment_results.tsv".format(output=output), 'a') as reliable_table:
-        reliable_table.write("Contig_ID\tReference:pident\tReference:align_length\t"
-                             "UniProt:pident\tUniProt:align_length\n")
+        reliable_table.write("Contig_ID\tReference:max_pident\tReference:max_align_length\t"
+                             "UniProt:max_pident\tUniProt:max_align_length\n")
         for contig in set(reliable):
+            ref_pident = [el["pident"] for el in ref_dict[contig]]
+            uniprot_pident = [el["pident"] for el in uniprot_dict[contig]]
+            ref_length = [el["length"] for el in ref_dict[contig]]
+            uniprot_length = [el["length"] for el in uniprot_dict[contig]]
+
             reliable_table.write("{id}\t{ref_pident}\t{ref_len}\t{uniprot_pident}\t{uniprot_len}\n".format(
-                id=contig, ref_pident=ref_dict[contig]["pident"], ref_len=ref_dict[contig]["length"],
-                uniprot_pident=uniprot_dict[contig]["pident"], uniprot_len=uniprot_dict[contig]["length"]
+                id=contig, ref_pident=max(ref_pident), ref_len=max(ref_length),
+                uniprot_pident=max(uniprot_pident), uniprot_len=max(uniprot_length)
             ))
 
 
@@ -62,4 +70,4 @@ if __name__ == "__main__":
     blast_tab_parsing(args.blastx_uniprot, uniprot_dict)
 
     contigs_selection(contigs_dict, ref_dict, uniprot_dict, reliable)
-    output_writing(args.output, reliable, contigs_dict, ref_dict, uniprot_dict)
+    output_writing(args.output, reliable, ref_dict, uniprot_dict)
