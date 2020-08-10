ccabb6f55198032abb1e4778a1f83cac0a309751
Maksim Nesterenko <maxnest@ro.ru>
2020-08-10 13:40:05 +0300

    Add files via upload

---
+++ run_BLASTP_for_phylostratr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+focal_fasta=$1
+uniprot_seqs=$2
+
+### MAIN ###
+
+for uniprot_fasta in $(find $uniprot_seqs -type f); do
+    tag=$(basename $uniprot_fasta)
+    echo "Comparison with $uniprot_fasta"
+    mkdir ${tag}_blast_out
+    cd ${tag}_blast_out
+    nohup makeblastdb -dbtype prot -in $uniprot_fasta -out $tag
+    wait
+    echo -e 'qseqid\tsseqid\tqstart\tqend\tsstart\tsend\tevalue\tscore' > ${tag}.tab
+    nohup blastp -query $focal_fasta -db $tag -num_threads 3 -outfmt '6 qseqid sseqid qstart qend sstart send evalue score' >> ${tag}.tab
+    wait
+    cd ..
+done
