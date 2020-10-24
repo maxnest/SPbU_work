6e4c15d6c9fec22f76f3badda30ced6e7e0ad0b7
Maksim Nesterenko <maxnest@ro.ru>
2020-10-24 18:09:45 +0300

    Add files via upload

--- run_DIAMOND_BLASTP_for_phylostratr.sh
+++ run_DIAMOND_BLASTP_for_phylostratr.sh
@@ -2,19 +2,18 @@
 
 focal_fasta=$1
 uniprot_seqs=$2
-threads=$3
 
 ### MAIN ###
 
 for uniprot_fasta in $(find $uniprot_seqs -type f); do
     tag=$(basename $uniprot_fasta)
     echo "Comparison with $uniprot_fasta"
-    mkdir ${tag}_blast_out
-    cd ${tag}_blast_out
-    nohup makeblastdb -dbtype prot -in $uniprot_fasta -out $tag
+    mkdir ${tag}_diamond_blast_out
+    cd ${tag}_diamond_blast_out
+    nohup diamond makedb --in $uniprot_fasta -d $tag
     wait
     echo -e 'qseqid\tsseqid\tqstart\tqend\tsstart\tsend\tevalue\tscore' > ${tag}.tab
-    nohup blastp -query $focal_fasta -db $tag -num_threads $threads -outfmt '6 qseqid sseqid qstart qend sstart send evalue score' >> ${tag}.tab
+    nohup diamond blastp -q $focal_fasta -d $tag --more-sensitive --threads 5 --outfmt 6 qseqid sseqid qstart qend sstart send evalue score >> ${tag}.tab
     wait
     cd ..
 done
